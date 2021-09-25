33bd7f2619b97aabc0da433645c20e813a8885a2
Dominik Obloza <main@dominikobloza.pl>
2021-09-25 03:38:32 +0200

    displaying next race

--- src/Components/Next.tsx
+++ src/Components/Next.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const Next: React.FC = () => {
-  return (
-    <div className="next">
-      <div className="next__title">Next race</div>
-      <div className="next__card">
-        <div className="next__date">2021-09-26</div>
-        <div className="next__name">Russian Grand Prix</div>
-      </div>
-    </div>
-  );
+    const [date, setdate] = useState('');
+    const [name, setname] = useState('');
+
+    useEffect(() => {
+        fetch('http://ergast.com/api/f1/current.json')
+            .then((response) => response.json())
+            .then((result) => {
+                setname(
+                    result.MRData.RaceTable.Races[
+                        findNext(result.MRData.RaceTable.Races)
+                    ].raceName
+                );
+                setdate(
+                    result.MRData.RaceTable.Races[
+                        findNext(result.MRData.RaceTable.Races)
+                    ].date
+                );
+            })
+            .catch((error) => console.log('error', error));
+    }, []);
+
+    const findNext = (data: { date: string }[]) => {
+        let now = new Date().toISOString().slice(0, 10);
+        let index = data.findIndex((e) => e.date > now);
+
+        return index;
+    };
+
+    return (
+        <div className="next">
+            <div className="next__title">Next race</div>
+            <div className="next__card">
+                <div className="next__date">{date}</div>
+                <div className="next__name">{name}</div>
+            </div>
+        </div>
+    );
 };
 
 export default Next;
